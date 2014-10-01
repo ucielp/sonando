0d0527689fd1a2168554f1c61f702ceab78fefbf
Jose Forte <bibaldo@gmail.com>
2014-10-01 15:14:19 -0300

    Landing page. First approach

---
+++ application/views/home/vistas/landing_view.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="X-UA-Compatible" content="ie=8" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="<?php echo base_url(); ?>images/favicon.gif" type="image/gif">
+
+<title><?php echo $title; ?></title>
+
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" type="text/css" >
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/landing.css" type="text/css" >
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/preinscripcion.css" type="text/css" >
+
+
+<!--[if IE]>
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/ie_style.css" type="text/css" >
+<![endif]-->	
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,60	0,700,800' rel='stylesheet' type='text/css'>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43175694-2', 'sonandoconelgol.com.ar');
+  ga('send', 'pageview');
+
+</script>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-27135950-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body class="bg_4">
+	<div class="header-landing">
+		<div id="top"></div>
+		<div class="logos-container">
+            <div class="nav-landing">
+                <ul>
+					<li>
+						<a class="logo-mujeres" href="<?php echo base_url(); ?>home">
+							<img src="<?php echo base_url(); ?>images/landing/logo_mujeres.png" width="150" alt="Libres">
+						</a>
+					</li>
+                    <li>
+						<a class="logo-inferiores" href="<?php echo base_url(); ?>home">
+							<img src="<?php echo base_url(); ?>images/landing/logo_inferiores.png" width="200" alt="Libres">
+						</a>
+					</li>
+					<li>
+						<a class="logo-libres" href="<?php echo base_url(); ?>home">
+							<img src="<?php echo base_url(); ?>images/landing/logo_libres.png" width="250" alt="Libres">
+						</a>
+					</li>
+					<li>
+						<a class="logo-empresas" href="<?php echo base_url(); ?>home">
+							<img src="<?php echo base_url(); ?>images/landing/logo_empresas.png" width="200" alt="Libres">
+						</a>
+					</li>
+					<li>
+						<a class="logo-mas-30" href="<?php echo base_url(); ?>home">
+							<img src="<?php echo base_url(); ?>images/landing/logo_mas_30.png" width="150" alt="Libres">
+						</a>
+					</li>
+					
+                </ul>
+            </div>
+		</div> <!-- END logos-container -->
+	</div>
+</body>
+
+</html>	
