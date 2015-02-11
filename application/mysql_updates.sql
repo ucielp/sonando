7b6d8d920d6f4055e456e4a5f746c5ab4190f346
uciel_laptop <uciel@uciel-samsung.(none)>
2015-02-11 11:52:07 -0300

    mysql updates

--- application/mysql_updates.sql
+++ application/mysql_updates.sql
@@ -1,1 +1,3 @@
 --~ ALTER TABLE jugadores ADD deslinde INT NOT NULL AFTER certificado
+
+ALTER TABLE `partidos` ADD `team1_pen` INT NULL DEFAULT NULL AFTER `team2_res`, ADD `team2_pen` INT NULL DEFAULT NULL AFTER `team1_pen`;
