012fe29c7cfaa0f2ce4987b48df9a51b6f7bb205
joseforte <bibaldo@gmail.com>
2015-07-10 16:24:04 -0300

    Fixture. Removed unused Goleadores data

--- application/views/home/vistas/fixture.php
+++ application/views/home/vistas/fixture.php
@@ -132,46 +132,6 @@
 		</div>
 		
 		<?php $this->view('home/temp/nav_bar');?>
-		
-		<div class="modal fade" id="modal-goleadores">
-			<div class="modal-dialog modal-md">
-				<div class="modal-content">
-					<div class="modal-header">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">GOLEADORES<span>COPA ARGENTINA, DE LA C</span></h4>
-					</div>
-					<div class="modal-body">
-						<table class="tablascg">
-							<thead>
-								<tr>
-									<th>Jugador</th><th>Equipo</th><th>Goles</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>Carlos Andrade</td><td>Ferrugem A.C.</td><td><strong>22</strong></td>
-								</tr>
-								<tr class="alt">
-									<td>Pepe Biondi</td><td>Sacachispa</td><td><strong>22</strong></td>
-								</tr>
-								<tr>
-									<td>German Rolon</td><td>Ficatucho</td><td><strong>22</strong></td>
-								</tr>
-								<tr class="alt">
-									<td>Carlos</td><td>Ferrugem A.C.</td><td><strong>22</strong></td>
-								</tr>
-								<tr>
-									<td>Carlos</td><td>Ferrugem A.C.</td><td><strong>22</strong></td>
-								</tr>
-								<tr class="alt">
-									<td>Carlos</td><td>Ferrugem A.C.</td><td><strong>22</strong></td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		</div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
